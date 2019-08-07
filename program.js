366b3293665c2430b1d0794f98f29cfaf56e004d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-07 15:37:51 -0500

    [+] 1: HELLO WORLD en consola

---
+++ program.js
@@ -0,0 +1,6 @@
+/** NodeSchool.io */
+
+/** 1. Escribe un programa que imprima el texto "HELLO WORLD" en consola
+  (stdout). */
+let msg = 'Hello World';
+console .log( msg .toUpperCase() );     // HELLO WORLD
