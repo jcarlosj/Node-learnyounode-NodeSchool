e0542e61a9c888965af4810b0ac8e40fea58811d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-07 15:53:06 -0500

    [+] 2: Sumar numeros pasados como argumentos por consola

--- program.js
+++ program.js
@@ -1,6 +1,13 @@
 /** NodeSchool.io */
 
-/** 1. Escribe un programa que imprima el texto "HELLO WORLD" en consola
-  (stdout). */
-let msg = 'Hello World';
-console .log( msg .toUpperCase() );     // HELLO WORLD
+/** 2. Escribe un programa que reciba uno o más números como argumentos de la
+ *     consola e imprima la suma de dichos números a consola (stdout).
+*/
+let suma = 0;
+console .log( process .argv );
+
+for( let i = 2; i < process .argv .length; i++ ) {
+    suma += Number( process .argv[ i ] );
+}
+
+console .log( suma );   // 329    Al ejecutar: $ node program.js 8 34 14 56 74 34 11 32 51 15
