8671f3b41834c8ef42db7bdc6aee311131a5ec77
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-07 18:01:02 -0500

    [+] 6: Modulariza el filtrado de archivos por extension de forma Asincrona

--- program.js
+++ program.js
@@ -1,31 +1,17 @@
 /** NodeSchool.io */
 
-/** 5.  Crea un programa que dado un directorio imprima una lista de archivos
- *      filtrados por la extensión. El primer argumento será la ruta al directorio
- *      (ej: '/path/dir/') y el segundo la extensión a filtrar, por ejemplo si
- *      recibes 'txt' deberás filtrar todos los archivos que terminen en .txt.
- * 
- *      Nota: el segundo argumento no incluye el punto '.'.
- * 
- *      La lista de archivos a imprimir en consola debe hacerse un archivo por
- *      línea y debes utilizar Async I/O.
+/** 6.  Modularizar el Filtrado de archivos por extensión de forma Asincrona 
+ *      Misma funcionalidad del Punto Anterior.
 */
-const fs = require( 'fs' ),
-      path = require( 'path' ),
-      EXT = '.txt';
-      
-fs .readdir( './', ( error, listFiles ) => {
-    console .log( listFiles );     
+const filter = require('./filter-files'),
+      pathFiles = './',
+      extension = '.txt';
 
-    listTxtFiles = listFiles. filter( file => {
-        return path .extname( file ) .toLowerCase() === EXT;
-    });
 
-    console .log( listTxtFiles );
-    
-    listTxtFiles .forEach( file => {
+filter( pathFiles, extension, ( error, files ) => {    // CallBack
+    console.log( ' > Ficheros', files );
+
+    files .forEach( file => {
         console .log( file );
     });
-
-
-});    
+});
